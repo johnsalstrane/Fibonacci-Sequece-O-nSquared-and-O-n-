6d512880ac372307d045bfd6ab39653e7b064606
johnsalstrane <79778754+johnsalstrane@users.noreply.github.com>
2022-01-19 11:49:14 -0800

    Made the output more readable

--- source.cpp
+++ source.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     for (int i = 0; i < num; i++)
     {
         result = FabinocciSequenceOn(i, array);
-        printf("%llu\n", result);
+        printf("%d: %llu\n", i+1, result);
     }
 
     return(0);
